27243386cc2a3626dacf78c5b2cc69d8e61047a2
eryoyo <15171271104@163.com>
2021-11-29 11:53:22 +0800

    增加了文件列表视图里面的小组件

---
+++ fileRecordView_finish.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QWidget
+
+# 填充在finish列表里面的widget
+class fileRecordView_finish(QWidget):
+    def __init__(self):
+        super(fileRecordView_finish, self).__init__()
+        
+    def setAction(self):
+        pass
---
+++ fileRecordView_wait.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QWidget
+
+
+# 填充在wait列表里面的widget
+class fileRecordView_finish(QWidget):
+    def __init__(self):
+        super(fileRecordView_finish, self).__init__()
+
+    def setAction(self):
+        pass
